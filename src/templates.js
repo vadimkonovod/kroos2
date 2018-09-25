d0f21d8f6054d1c8469cf2f2b07ccb6a45691aae
Dmitry Lobanov <dmitrylo@playtika.com>
2018-09-25 18:15:59 +0300

    Visual improvements

--- src/templates.js
+++ src/templates.js
@@ -9,7 +9,7 @@ function statusMsg(event) {
   const hasUsers = event.users.length > 0;
 
   const priceMsg = `${event.price} BYN` + (hasUsers ? `- per person ${event.pricePerUser()}` : '');
-  const participantsMsg = hasUsers ? `Participants ${event.users.length}` + usersMsg(event) : 'Event doesn\'t have any users';
+  const participantsMsg = hasUsers ? `Participants ${event.users.length}\n` + usersMsg(event) : 'Event doesn\'t have any users';
 
   return dateMsg(event.date) + '\n' + priceMsg + '\n\n' + participantsMsg;
 }
